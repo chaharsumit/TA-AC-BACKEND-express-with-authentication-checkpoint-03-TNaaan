954db1fd3b5bf5fdfe423de9604b1256c5e037f9
Sumit Chahar <chahar.sumit888@gmail.com>
2021-12-30 13:46:19 +0530

    Add Schema for users

--- block-BNaafu/expense-tracker/routes/index.js
+++ block-BNaafu/expense-tracker/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['openid', 'email', 'profile'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/failure'}), (req, res) => {
+  res.redirect('/success');
+})
+
+router.get('/auth/github', passport.authenticate('github'));
+
+router.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/failure'}), (req, res) => {
+  res.redirect('/success');
+})
 
 module.exports = router;
